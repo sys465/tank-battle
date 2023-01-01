e99040179b7efdbc41a901ebfa545f0b50ef4f41
sys465 <sys465@yandex.ru>
2023-01-01 19:27:33 +0300

    Fixed message

--- src/main/java/com/github/sys465/tankbattle/Main.java
+++ src/main/java/com/github/sys465/tankbattle/Main.java
@@ -2,6 +2,6 @@ package com.github.sys465.tankbattle;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, Tank Battle!");
     }
 }
